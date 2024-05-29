f1f445c9231ad35ccdf40b09e2792ef874e1e942
Goduu <idcangussu@hotmail.com>
2024-05-29 15:39:41 +0200

    add code mirror

--- initial/frontend/src/components/RequestAnswer.tsx
+++ initial/frontend/src/components/RequestAnswer.tsx
@@ -1,15 +1,20 @@
-import React, { FC } from 'react'
+"use client"
+import React, { FC, useRef } from 'react'
 import { AnswerObject } from './AnswerObject';
+import CodeMirror from '@uiw/react-codemirror';
+import { java } from '@codemirror/lang-java';
 
 export type RequestAnswerProps = {
     answer: AnswerObject;
 }
 
 export const RequestAnswer: FC<RequestAnswerProps> = ({ answer }) => {
+    const editorRef = useRef(null);
+
     return (
-        <>
+        <div className='flex flex-col gap-2'>
             <div
-                className="lg:max-w-5xl w-full h-48 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="lg:max-w-5xl w-full h-auto group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
                 <h2 className={`mb-3 text-2xl font-semibold`}>
                     Answer
@@ -17,14 +22,14 @@ export const RequestAnswer: FC<RequestAnswerProps> = ({ answer }) => {
                 {answer.answer}
             </div>
             <div
-                className="lg:max-w-5xl w-full h-48 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="lg:max-w-5xl w-full h-auto group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
                 <h2 className={`mb-3 text-2xl font-semibold`}>
                     Code
                 </h2>
-                {answer.code}
+                <CodeMirror value={answer.code} ref={editorRef} height={"auto"} theme={"dark"} extensions={[java()]} />
             </div>
-        </>
+        </div>
 
     )
 }
